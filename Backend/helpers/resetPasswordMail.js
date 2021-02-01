04a67589fbce219cf38c67c18f03a35b300847b0
UltraNote <support@ultranote.org>
2021-02-01 06:11:34 +0100

    Update resetPasswordMail.js

--- Backend/helpers/resetPasswordMail.js
+++ Backend/helpers/resetPasswordMail.js
@@ -15,7 +15,7 @@ exports.ResetPasswordMail = async(newUser, token) => {
     const mailOptions = {
         from: 'support@ultranote.org',
         to: newUser.mail,
-        subject: 'ULTRANOT: reset password',
+        subject: 'ULTRANOT CLOUD: reset password',
         text: 'Hello' + newUser.firstName + newUser.lastName ,
 		html: 
 			`<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
