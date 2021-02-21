dbb3825f5009ee490d324e86193bb8e399bf4d00
UltraNote <support@ultranote.org>
2021-02-21 03:34:49 +0100

    revert

--- cryptwallet/app/components/ForgotPasswordPage/index.js
+++ cryptwallet/app/components/ForgotPasswordPage/index.js
@@ -19,7 +19,6 @@ import Logo from '../../images/logo_512x512.png';
 
 import '../SignupPage/account.scss'
 import { toast } from 'react-toastify';
-import ReCAPTCHA from "react-google-recaptcha";
 import { RessetPassword } from '../../containers/RessetPassword';
 import { requestEmailResetStart, resetPasswordStart } from '../../store/auth/auth.actions';
 import { connect } from 'react-redux';
@@ -34,7 +33,6 @@ class ForgotPasswordPage extends Component {
         mode: 'reset',
         email: '',
         error: {},
-        verifiedCaptcha: null
         token
       };
     }
@@ -230,11 +228,7 @@ class ForgotPasswordPage extends Component {
 
   }
 
-  onChangeCap = value => {
-    this.setState({
-      verifiedCaptcha: value
-    });
-  }
+
   render() {
     return (
       <Fragment>
@@ -269,10 +263,6 @@ class ForgotPasswordPage extends Component {
                     value={this.state.email}
                     helperText={this.state.error.email ? this.state.error.email : ""}
                   />
-                  <ReCAPTCHA className="recaptcha"
-                      sitekey="6LdqlfoZAAAAAMLxltM3BSoqaFQInUh_lxtZ88cC"
-                      onChange={this.onChangeCap}
-                    />
                   <Button
                     type="submit"
                     className="submitButton"
@@ -303,10 +293,8 @@ class ForgotPasswordPage extends Component {
                     />
                     <Button
                       type="submit"
-                      className="submitButton">
-                   <Button type="submit" className="submitButton" disabled={!this.state.verifiedCaptcha}>
-                   Send
-                   </Button>
+                      className="submitButton"
+                    >Send</Button>
                   </Form>)
                 }
               </Grid>
