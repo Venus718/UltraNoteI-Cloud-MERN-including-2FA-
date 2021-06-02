d732886625da2f4293c45aeb8701254d94a03fdc
zelalem-12 <zelalem.antigegn12@gmail.com>
2021-06-02 21:38:04 +0300

    separating the backend admin serer from its frontend

--- cryptwallet-admin/server/routes/dashboard.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const dashboard = require('../controllers/dashboard');
-
-router.post('/', dashboard);
-
-module.exports = router;
