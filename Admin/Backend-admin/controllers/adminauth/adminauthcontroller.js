e9e94821b43f8d4204f4a1861a57b507a636905f
hussnain hashmi <hussnain77133@gmail.com>
2022-04-13 14:08:51 +0500

    changed the email sending logic from 5 in 1 min to 1 email in 1 min

--- Admin/Backend-admin/controllers/adminauth/adminauthcontroller.js
+++ Admin/Backend-admin/controllers/adminauth/adminauthcontroller.js
@@ -955,11 +955,14 @@ exports.post_mass_email = async (req, res, next) => {
   try {
     let { subject, message, users } = req.body;
     let chunks = [];
-    let chunkSize = 5;
+    let chunkSize = 1;
     for (let i = 0; i < users.length; i += chunkSize) {
       chunks.push(users.slice(i, i + chunkSize));
     }
-    const delay = () => new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const delay = async () =>
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
     const sendEmail = async (chunk) => {
       for (let i = 0; i < chunk.length; i++) {
         const mailOptions = {
