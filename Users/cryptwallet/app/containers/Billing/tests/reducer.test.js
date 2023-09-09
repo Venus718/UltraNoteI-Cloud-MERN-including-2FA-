6f63a116c20e93ed5096543eeebfd7f51b344997
UltraNote <support@ultranote.org>
2023-09-10 00:58:43 +0200

    Revert "Revert "Chat changes""

--- Users/cryptwallet/app/containers/Billing/tests/reducer.test.js
+++
@@ -1,8 +0,0 @@
-import { fromJS } from 'immutable';
-import billingReducer from '../reducer';
-
-describe('billingReducer', () => {
-  it('returns the initial state', () => {
-    expect(billingReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
--- Users/cryptwallet/app/containers/Chat/reducer.js
+++ Users/cryptwallet/app/containers/Chat/reducer.js
@@ -1,6 +1,6 @@
 /*
  *
- * Billing reducer
+ * chat reducer
  *
  */
 
@@ -9,7 +9,7 @@ import { DEFAULT_ACTION } from './constants';
 
 export const initialState = fromJS({});
 
-function billingReducer(state = initialState, action) {
+function chatReducer(state = initialState, action) {
   switch (action.type) {
     case DEFAULT_ACTION:
       return state;
@@ -18,4 +18,4 @@ function billingReducer(state = initialState, action) {
   }
 }
 
-export default billingReducer;
+export default chatReducer;
